7df8bd3d0c6ffe7530b87bfd56311b1732e114b3
renaultluk <71740121+renaultluk@users.noreply.github.com>
2021-04-06 13:55:49 +0800

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,3 +1,5 @@
+divideList = []
+
 def gcd(x,y):
     quotient = 0
     remainder = 0
@@ -6,10 +8,20 @@ def gcd(x,y):
     else:
         quotient = x//y
         remainder = x%y
-        # divideList.append([x,y,quotient,remainder])
+        divideList.append([x,y,quotient,remainder])
         print("{divide} = {divisor} * {quot} + {remain}".format(divide=x, divisor=y, quot=quotient, remain=remainder))
         return gcd(y, remainder)
+
+def bezout():
+    print("Rewriting: ")
+    for i in divideList:
+        print("{remain} = {divide} - {divisor} * {quot}".format(remain=i[3], divide=i[0], divisor=i[1], quot=i[2]))
+     
+    count = len(divideList) - 2
+    while count != 0:
+        
 
 num1 = int(input("Enter the larger number: "))
 num2 = int(input("Enter the smaller number: "))
 print("The GCD is: {}".format(gcd(num1, num2)))
+bezout()
